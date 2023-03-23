278e5c82a7fc97b87575c4aceaf0b5013c2648e1
Thomas Sanchez <sanchez.thomas056@gmail.com>
2023-03-23 10:54:06 +0100

    Add dir_cosines to calculate the orientation of the s/q-form matrix.

--- src/nifti.py
+++ src/nifti.py
@@ -1,6 +1,14 @@
 import numpy as np
+import nibabel as nb
 
 
 def voxel_sizes(xform):
     """Calculates the voxel sizes given the s/q-form matrix of a NIfTI file."""
     return np.linalg.norm(xform[:-1, :-1], axis=0)
+
+
+def dir_cosines(xform):
+    """Calculates the orientation of the s/q-form matrix of a NIfTI file."""
+    return xform[:-1, :-1] / voxel_sizes(xform)
+
+
